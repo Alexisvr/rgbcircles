6662b77d4628cc100211823694810ce4c986a771
vorobev <alex18v@mail.ru>
2017-04-18 17:43:49 +0300

    Урок 8

--- app/src/main/java/com/example/rgbcircles/EnemyCircle.java
+++ app/src/main/java/com/example/rgbcircles/EnemyCircle.java
@@ -10,7 +10,8 @@ import java.util.Random;
 public class EnemyCircle extends SimpleCircle {
     public static final int FROM_RADIUS = 10;
     public static final int TO_RADIUS = 100;
-    public static final int EMEMY_CIRCLE = Color.RED;
+    public static final int EMEMY_COLOR = Color.RED;
+    public static final int FOOD_COLOR = Color.rgb(0, 200, 0);
 
     public EnemyCircle(int x, int y, int radius) {
         super(x, y, radius);
@@ -22,7 +23,21 @@ public class EnemyCircle extends SimpleCircle {
         int y = random.nextInt(GameManager.getHight());
         int raduis = FROM_RADIUS + random.nextInt(TO_RADIUS - FROM_RADIUS);
         EnemyCircle enemyCircle = new EnemyCircle(x, y, raduis);
-        enemyCircle.setColor(EMEMY_CIRCLE);
         return enemyCircle;
+    }
+
+    public void setEnemyOrFoodColorDependsOn(MainCircle mainCircle) {
+        if (isSmallerThan(mainCircle)) {
+            setColor(FOOD_COLOR);
+        } else {
+            setColor(EMEMY_COLOR);
+        }
+    }
+
+    private boolean isSmallerThan(SimpleCircle circle) {
+        if (radius < circle.radius) {
+            return true;
+        } else
+            return false;
     }
 }
